baf7cba05b15ca0811a0bafab25323b3e2f88985
dldemoks <demokstar@gmail.com>
2017-02-07 19:50:21 +0700

    update

--- Payeer (Joomla 3.4.8 - Virtuemart 3.0.9) ru, en/payeer.php
+++ Payeer (Joomla 3.4.8 - Virtuemart 3.0.9) ru, en/payeer.php
@@ -169,6 +169,18 @@ class plgVmpaymentPayeer extends vmPSPlugin
 					$err = true;
 				}
 				
+				$db = JFactory::getDBO();
+				$q = "SELECT order_status FROM #__virtuemart_orders WHERE `virtuemart_order_id`='" . $virtuemart_order_id . "'";
+				$db->setQuery($q);
+				$db->query();
+				$order_status = $db->loadResult();
+
+				if (!$order_status)
+				{
+					$message .= " order does not exist\n";
+					$err = true;
+				}
+				
 				// проверка статуса
 				
 				if (!$err)
@@ -176,14 +188,24 @@ class plgVmpaymentPayeer extends vmPSPlugin
 					switch ($payeer_data['m_status'])
 					{
 						case 'success':
-							$order['order_status'] = $method->status_success;
-							$modelOrder->updateStatusForOneOrder($virtuemart_order_id, $order, true);
+						
+							if ($order_status != $method->status_success)
+							{
+								$order['order_status'] = $method->status_success;
+								$modelOrder->updateStatusForOneOrder($virtuemart_order_id, $order, true);
+							}
+							
 							break;
 							
 						default:
+						
+							if ($order_status != $method->status_canceled)
+							{
+								$order['order_status'] = $method->status_canceled;
+								$modelOrder->updateStatusForOneOrder($virtuemart_order_id, $order, true);
+							}
+
 							$message .= " The payment status is not success\n";
-							$order['order_status'] = $method->status_canceled;
-							$modelOrder->updateStatusForOneOrder($virtuemart_order_id, $order, true);
 							$err = true;
 							break;
 					}
